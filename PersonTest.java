50d00237464905e312ed5b6f4ac00b51091e4762
igalna <igalna@gmail.com>
2014-11-05 15:54:27 +0000

    Second Commit

--- PersonTest.java
+++ PersonTest.java
@@ -5,10 +5,30 @@ public class PersonTest {
     
     @Test
     public void testsNormalName() {
-        Person p = new Person();
+        Person first = new Person();
         String input = "Dereck Robert Yssirt";
-        String output = p.getInitials(input);
+        String output = first.getInitials(input);
         String expected = "DRY";
+        assertEquals(output, expected);
+        
+        Person second = new Person();
+        String input = "Roger      Smith           Johnson";
+        System.out.println(input);
+        String output = second.getInitials(input);
+        System.out.println(output);
+        String expected = "RSJ";
+        assertEquals(output, expected);
+    }
+    
+    
+    @Test
+    public void testWhiteSpaceName() {
+        Person third = new Person();
+        String input = "I  J  W";
+        System.out.println(input);
+        String output = third.getInitials(input);
+        System.out.println(output);
+        String expected = "IJW";
         assertEquals(output, expected);
     }
 }
